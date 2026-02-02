e931e9feafc0abcb279c649da003e91c08ed9c2c
Vaishnavi Sable <vaishnavi.d.sable@cumminscollege.in>
2026-02-02 11:54:02 +0530

    Initial Calculator Structure

---
+++ Calculator.java
@@ -0,0 +1,22 @@
+class Calculator {
+
+    static int add(int a, int b) {
+        return 0;
+    }
+
+    static int sub(int a, int b) {
+        return 0;
+    }
+
+    static int mul(int a, int b) {
+        return 0;
+    }
+
+    static int div(int a, int b) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        // will call all functions later
+    }
+}
